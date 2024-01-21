397b371be88213d31f26c3e89f9bb73e13300c46
mahdi alaee <xximahdixx@gmail.com>
2024-01-21 19:33:40 +0330

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-4xl">initial project</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{jsx,js}", "./index.html"],
+  theme: {
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      "2xl": "1400px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["barlow", "sans-serif"],
+      },
+    },
+    plugins: [],
+  },
+};
